5345c423fc9ad1ffdc0bdef8741c6686c788e64c
Ajay <aky11052003@gmail.com>
2023-08-24 23:26:08 +0530

    updated page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import Image from 'next/image'
 import { Glow } from '@/assests'
-import { Navbar, Prize, Organizers, Timeline, FAQ } from '@/components'
+import { Navbar, Prize, Organizers, Timeline, FAQ, About, Hero, Sponsors } from '@/components'
 
 export default function Home() {
   return (
     <div className=' bg-[#1E1E1E]'>
     {/*<Image src={Glow} width={400} height={400} /> */} {/* This is how you should use image*/}
     <Navbar/>
+    <Hero/>
+    <About/>
     <Prize/>
     <Timeline/>
+    <Sponsors/>
     <FAQ/>
     <Organizers/>
     </div>

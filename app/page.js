9c532658bad55d3e4998fb60a00dad20dc275adf
Ajay <aky11052003@gmail.com>
2023-08-25 12:00:15 +0530

    made hero responsive

--- app/page.js
+++ app/page.js
@@ -18,17 +18,35 @@ export default function Home() {
         src={Glow}
         width={400}
         height={400}
-        className="absolute top-10 md:left-1/3 z-0 overflow-hidden"
+        className="absolute top-12 md:left-1/3 z-0 overflow-hidden rotate-180"
+      />
+      <Image
+        src={Glow}
+        width={400}
+        height={400}
+        className="absolute top-[48%] -left-10 z-0 overflow-hidden rotate-90"
+      />
+      <Image
+        src={Glow}
+        width={400}
+        height={400}
+        className="absolute top-[38%] right-0 z-0 overflow-hidden -rotate-90"
+      />
+      <Image
+        src={Glow}
+        width={400}
+        height={400}
+        className="absolute top-[85%] right-0 md:right-[30%] lg:right-[40%] z-0 overflow-hidden "
       />
       <div className="z-10 relative">
-      <Navbar />
-      <Hero />
-      <About />
-      <Prize  />
-      <Timeline />
-      <Sponsors />
-      <FAQ />
-      <Organizers />
+        <Navbar />
+        <Hero />
+        <About />
+        <Prize />
+        <Timeline />
+        <Sponsors />
+        <FAQ />
+        <Organizers />
       </div>
     </div>
   );
--- components/Hero.js
+++ components/Hero.js
@@ -1,50 +1,49 @@
-import { MedicalShield, Arduino, RoboticArm} from "@/assests";
+import { MedicalShield, Arduino, RoboticArm } from "@/assests";
 import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col justify-around items-center align-middle pt-5 h-30em p-20">
+    <div className="relative flex flex-col justify-around items-center align-middle h-30em lg:mt-5 lg:m-20 h-fit">
+      <div className=""></div>
 
-    <div className=""></div>
-
-    <div className="absolute z-10 top-0 left-0 ">
+      <div className="absolute z-10 top-0 left-0 h-20 w-24 md:h-32 md:w-40">
         <Image
-        className="mx-0 my-95"
-        src={MedicalShield}
-        height={209}
-        width={201}
-        alt="MedShield"
-      />
+          className="mx-0 my-95"
+          src={MedicalShield}
+          alt="MedShield"
+        />
+      </div>
+
+      <div className="absolute z-10 top-0 right-0 h-20 w-24 md:h-32 md:w-40">
+        <Image
+          className="mx-0 my-95"
+          src={Arduino}
+          alt="Arduino"
+        />
+      </div>
+
+      <div className="absolute z-10 bottom-10 right-0 h-20 w-24 md:h-32 md:w-40">
+        <Image
+          className="mx-0 my-95"
+          src={RoboticArm}
+          alt="RoboticArm"
+        />
+      </div>
+
+      <div className="flex text-white text-5xl md:text-7xl font-bold tracking-{5%} w-{8px} px-23 h-{177px} mx-3 mt-20 translate-y-204 justify-center align-middle text-center">
+       
+        HACKING HEALTH
+      </div>
+      <div className="flex bg-[#C086EA] text-white mb-10 mt-20 p-2 rounded-2xl text-xl font-semibold">
+        {" "}
+        <a href="placeholder-link">Register</a>{" "}
+      </div>
+      <div className="flex text-white text-xl font-semibold">
+        {" "}
+        <a href="placeholder-link" >Join <span className=" italic underline">Discord</span></a>{" "}
+      </div>
     </div>
+  );
+};
 
-    <div className="absolute z-10 top-0 right-0 ">
-    <Image
-    className="mx-0 my-95"
-    src={Arduino}
-    height={139.41}
-    width={159.71}
-    alt="Arduino"
-  />
-</div>
-
-<div className="absolute z-10 bottom-10 right-0">
-<Image
-className="mx-0 my-95"
-src={RoboticArm}
-height={198.67}
-width={198.67}
-alt="RoboticArm"
-/>
-</div>
-    
-
-
-
-    <div className="flex text-white text-7xl font-bold tracking-{5%} w-{8px} px-23 h-{177px} mx-3 mt-20 translate-y-204 "> CMC HACKATHON</div>
-    <div className="flex bg-[#C086EA] text-white mb-10 mt-20 p-2 rounded-2xl text-xl font-semibold"> <a href="placeholder-link">Register</a> </div>
-    <div className="flex text-white text-xl font-semibold"> <a href="placeholder-link">Join Discord</a> </div>
-    </div>
-  )
-}
-
-export default Hero
+export default Hero;
